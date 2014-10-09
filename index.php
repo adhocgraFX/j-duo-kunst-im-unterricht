c607290117fcb2fa3b419f3a01a22837aa7747a4
adhocgraFX <info@adhocgrafx.de>
2014-10-09 21:49:49 +0200

    Update index.php

--- index.php
+++ index.php
@@ -57,7 +57,7 @@ $doc->addScript($tpath.'/js/modernizr-2.8.2.min.js');
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <!-- typekit fonts -->
-    <script type="text/javascript" src="//use.typekit.net/gbu8wbh.js"></script>
+    <script type="text/javascript" src="//use.typekit.net/xyz.js"></script>
     <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
     <!-- Bildverkleinerung über mobify cdn -->
